77dc4570fa43975c154839745d8f163c5f0fc0b1
aayush262 <aayushlama26@gmail.com>
2020-04-17 14:45:57 +0545

    s

--- controllers/mail.js
+++ controllers/mail.js
@@ -73,7 +73,7 @@ module.exports={
             
             </div></div>`
             await sendMail({
-                email:'aayushlama26@gmail.com',
+                email:'angeltechnepal@gmail.com',
                 subject:'From AngelTech Website',
                 html
             })
